9955a8500a0dc236b3aef153ea56390f2302b8c9
Jason Pompon <jasonpompon@gmail.com>
2017-02-20 10:20:23 +1100

    set up materialize to work with sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,21 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 
-gulp.task('test', function() {
-  console.log('running');
-});
-
 gulp.task('server', function() {
   connect.server();
 });
 
+// convert .scss file convert is to css and send it to the css folder(dest)
 gulp.task('sass', function() {
-  gulp.src('style.scss')
+  gulp.src('sass/**/*.scss')
     .pipe( sass() )
     .pipe( gulp.dest('css') );
 });
 
+// watch any changes file that end .scss from the sass folder
 gulp.task('watch', function() {
-  gulp.watch(['style.scss'], ['sass']);
+  gulp.watch(['sass/**/*.scss'], ['sass']);
 });
 
+// default to when we type "gulp" on the command line.
 gulp.task('default', ['server', 'watch']);
